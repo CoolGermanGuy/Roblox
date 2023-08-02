199aea5736cc041f343eaf8535241a2e0c275ff0
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-08-02 02:07:00 +0200

    Update DrawingLibrary_funny.lua

--- DrawingLibrary_funny.lua
+++ DrawingLibrary_funny.lua
@@ -12,7 +12,7 @@ HueOffset = 0
 Drawing1.NumSides = 32
 Drawing1.Radius = 150
 --                             |        X       |       Y            |
-Drawing1.Position = Vector2.new(0.85 * (VPS.X / 2),  1.4 * (VPS.Y / 2))
+Drawing1.Position = Vector2.new(0.85 * (VPS.X / 2),  1.55 * (VPS.Y / 2))
 Drawing1.Thickness = 4
 Drawing1.Filled = true
 Drawing1.Visible = true
@@ -22,17 +22,17 @@ Drawing1.Transparency = 1
 Drawing2.NumSides = 32
 Drawing2.Radius = 150
 --                             |        X       |       Y            |
-Drawing2.Position = Vector2.new(1.15 * (VPS.X / 2),  1.4 * (VPS.Y / 2))
+Drawing2.Position = Vector2.new(1.15 * (VPS.X / 2),  1.55 * (VPS.Y / 2))
 Drawing2.Thickness = 4
 Drawing2.Filled = true
 Drawing2.Visible = true
 Drawing2.Color = Color3.fromRGB(1,1,1)
 Drawing2.Transparency = 1
 --                             |        X       |       Y            |
-Drawing3.PointA = Vector2.new(0.884 * (VPS.X / 2), 0.4 * (VPS.Y / 2))
-Drawing3.PointB = Vector2.new(1.116 * (VPS.X / 2), 0.4 * (VPS.Y / 2))
-Drawing3.PointC = Vector2.new(1.116 * (VPS.X / 2), 1.4 * (VPS.Y / 2))
-Drawing3.PointD = Vector2.new(0.884 * (VPS.X / 2), 1.4 * (VPS.Y / 2))
+Drawing3.PointA = Vector2.new(0.884 * (VPS.X / 2), 0.55 * (VPS.Y / 2))
+Drawing3.PointB = Vector2.new(1.116 * (VPS.X / 2), 0.55 * (VPS.Y / 2))
+Drawing3.PointC = Vector2.new(1.116 * (VPS.X / 2), 1.55 * (VPS.Y / 2))
+Drawing3.PointD = Vector2.new(0.884 * (VPS.X / 2), 1.55 * (VPS.Y / 2))
 Drawing3.Thickness = 4
 Drawing3.Filled = true
 Drawing3.Visible = true
@@ -42,7 +42,7 @@ Drawing3.Transparency = 1
 Drawing4.NumSides = 32
 Drawing4.Radius = 150
 --                             |        X       |       Y            |
-Drawing4.Position = Vector2.new(VPS.X / 2,  0.4 * (VPS.Y / 2))
+Drawing4.Position = Vector2.new(VPS.X / 2,  0.55 * (VPS.Y / 2))
 Drawing4.Thickness = 4
 Drawing4.Filled = true
 Drawing4.Visible = true
