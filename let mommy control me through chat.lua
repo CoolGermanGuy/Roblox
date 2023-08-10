ab6ba4837efb4b77c5e2d9ec13ccf006cc4a362f
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-08-10 22:30:31 +0200

    Update let mommy control me through chat.lua

--- let mommy control me through chat.lua
+++ let mommy control me through chat.lua
@@ -1,4 +1,4 @@
-local selected = "Cool_Adri89"
+local selected = "mew_Sp"
 local prefix = "!"
 
 
@@ -9,6 +9,10 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local controlList = {Players[selected]}
 
 allcontrol = false
+
+-- variables
+loopteleport = false
+
 
 function getArgs(splitmsg)
 	local inString = false
@@ -38,45 +42,86 @@ function getArgs(splitmsg)
 	return args
 end
 
+function getPlayer(pattern: string)
+	if string.match(pattern, "Me") or string.match(pattern, "me") then
+		return selected;
+	elseif string.match(pattern, "All") or string.match(pattern, "all") then
+		return Players:GetPlayers();
+	else
+		for index, player in ipairs(Players:GetPlayers()) do
+			if player.Name:match(pattern) or player.DisplayName:match(pattern) then
+				return player;
+			end
+		end
+
+		return nil;
+	end
+end
+
 ------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------
 
 local commands = {
-    ["summon"] = function()
+    ["summon"] = {func = function()
         LocalPlayer.Character.HumanoidRootPart.CFrame = Players[selected].Character.HumanoidRootPart.CFrame
-    end,
-    ["say"] = function(...)
+    end, aliases = {"bring", "come", "here"}},
+    ["say"] = {func = function(...)
         ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(table.concat({...}, " "), "All")
-    end,
-    ["jump"] = function()
+    end, aliases = {"talk", "speak"}},
+    ["jump"] = {func = function()
         LocalPlayer.Character.Humanoid.Jump = true
-    end,
-    ["die"] = function()
+    end, aliases = {"bounce"}},
+    ["die"] = {func = function()
         LocalPlayer.Character.Head:Destroy()
-    end,
-    ["allcontrol"] = function(bool)
+    end, aliases = {"death", "oof"}},
+    ["goto"] = {func = function(player)
+        print(player)
+        player = getPlayer(player)
+        LocalPlayer.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame
+    end, aliases = {"tp", "teleport"}},
+    ["loopgoto"] = {func = function(bool, player)
         if bool == "true" then
-            allcontrol = true
-            for i, v in ipairs(Players:GetPlayers()) do
-                if v.Name ~= selected then
-                    table.insert(controlList, v)
+            loopteleport = true
+            player = getPlayer(player)
+            while task.wait() do
+                if loopteleport then
+                    LocalPlayer.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame
                 end
             end
         elseif bool == "false" then
-            for i, v in ipairs(controlList) do
-                if v.Name ~= selected then
-                    table.remove(controlList, i)
-                end
-            end
+            loopteleport = false
         else 
             ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(bool.." is not valid, use either \"true\" or \"false\"", "All")
         end
-    end,
-    ["playmusic"] = function(id)
-        local id = "https://www.roblox.com/asset/?id="..tonumber(id)
-        ReplicatedStorage.Remotes.Inventory.PlaySong:FireServer(id)
-    end,
+    end, aliases = {"loopgoto", "looptp", "lp", "lg"}},
+    ["addcontrol"] = {func = function(player)
+        player = getPlayer(player)
+        table.insert(controlList, player)
+    end, aliases = {"addc", "ac", "controladd", "controla"}},
+    ["removecontrol"] = {func = function(player)
+        player = getPlayer(player)
+        table.remove(controlList, table.find(controlList, player))
+    end, aliases = {"removec", "rc", "controlremove", "controlr"}}
+    --[[
+    ["allcontrol"] = func = {function(bool)
+            if bool == "true" then
+                allcontrol = true
+                for i, v in ipairs(Players:GetPlayers()) do
+                    if v.Name ~= selected then
+                        table.insert(controlList, v)
+                    end
+                end
+            elseif bool == "false" then
+                for i, v in ipairs(controlList) do
+                    if v.Name ~= selected then
+                        table.remove(controlList, i)
+                    end
+                end
+            else 
+                ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(bool.." is not valid, use either \"true\" or \"false\"", "All")
+        end, aliases = {"acontrol"}},
+    ]]--
 }
 ------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------
@@ -89,8 +134,12 @@ for i, v in ipairs(controlList) do
             local splitmsg = string.split(prefixsplitmsg[2], " ")
             local args = getArgs(splitmsg)
             local lowercommand = string.lower(splitmsg[1])
-            if commands[lowercommand] then
-                commands[lowercommand](table.unpack(args))
+
+            for command, data in pairs(commands) do
+                if lowercommand == command or (data.aliases and table.find(data.aliases, lowercommand)) then
+                    data.func(table.unpack(args))
+                    break
+                end
             end
         end
     end)
@@ -101,3 +150,7 @@ Players.PlayerAdded:Connect(function(player)
         table.insert(controlList, player)
     end
 end)
+
+
+LocalPlayer.Character.HumanoidRootPart.CFrame = Players[selected].Character.HumanoidRootPart.CFrame
+ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("At your service, "..Players[selected].DisplayName, "All")
