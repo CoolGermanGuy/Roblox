495e24e4c7da5c636c2fd34dec524d2abf434d97
CoolGermanGuy <EmailCGG@protonmail.com>
2025-03-17 20:13:16 +0100

    Update MM2 v2.lua

--- MM2 v2.lua
+++ MM2 v2.lua
@@ -13,21 +13,21 @@ local BackgroundFrame = Instance.new("Frame") -- create BackgroundFrame
 BackgroundFrame.Name = "BackgroundFrame"
 BackgroundFrame.BackgroundColor3 = Color3.fromRGB(38, 38, 38) 
 local BackgroundFrameUICorner = Instance.new("UICorner")
-BackgroundFrameUICorner.CornerRadius = UDim2.new(0, 10)
+BackgroundFrameUICorner.CornerRadius = UDim.new(0, 10)
 BackgroundFrameUICorner.Parent = BackgroundFrame
 BackgroundFrame.Parent = ScreenGui
 
 local HolderFrame = Instance.new("Frame") -- create HolderFrame
 local UIGridLayout = Instance.new("UIGridLayout") -- manages player UI
-UIGridLayout.CellPadding = {0, 10, 0, 7}
-UIGridLayout.CellSize = {0.5, -5, 0, 50}
+UIGridLayout.CellPadding = UDim2.new(0, 10, 0, 7)
+UIGridLayout.CellSize = UDim2.new(0.5, -5, 0, 50)
 UIGridLayout.Parent = HolderFrame
 
 for i = 0, 12, 1 do -- add UI for each player
     local PlayerFrame = Instance.new("Frame")
-    PlayerFrame.BackgroundColor = Color3.fromRGB(94,94,94)
+    PlayerFrame.BackgroundColor3 = Color3.fromRGB(94,94,94)
     local PlayerFrameUICorner = Instance.new("UICorner")
-    PlayerFrameUICorner.CornerRadius = UDim2.new(0, 8)
+    PlayerFrameUICorner.CornerRadius = UDim.new(0, 8)
     PlayerFrameUICorner.Parent = PlayerFrame
 end
 HolderFrame.Parent = BackgroundFrame
