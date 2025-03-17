4c5419a5feec92add8307bd2a1bd1dabcd3607a4
CoolGermanGuy <EmailCGG@protonmail.com>
2025-03-17 20:10:12 +0100

    Update MM2 v2.lua

--- MM2 v2.lua
+++ MM2 v2.lua
@@ -11,19 +11,22 @@ ScreenGui.Parent = LocalPlayer.PlayerGui
 
 local BackgroundFrame = Instance.new("Frame") -- create BackgroundFrame
 BackgroundFrame.Name = "BackgroundFrame"
-BackgroundFrame.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
-Instance.new("UICorner").Parent = BackgroundFrame -- add UICorner
+BackgroundFrame.BackgroundColor3 = Color3.fromRGB(38, 38, 38) 
+local BackgroundFrameUICorner = Instance.new("UICorner")
+BackgroundFrameUICorner.CornerRadius.Offset = 10
+BackgroundFrameUICorner.Parent = BackgroundFrame
 BackgroundFrame.Parent = ScreenGui
 
-local HolderFrame = Instance.new("Frame")
-local UIGridLayout = Instance.new("UIGridLayout")
+local HolderFrame = Instance.new("Frame") -- create HolderFrame
+local UIGridLayout = Instance.new("UIGridLayout") -- manages player UI
 UIGridLayout.CellPadding = {0, 10, 0, 7}
 UIGridLayout.CellSize = {0.5, -5, 0, 50}
 UIGridLayout.Parent = HolderFrame
 
-for i = 0, 12, 1 do
+for i = 0, 12, 1 do -- add UI for each player
     local PlayerFrame = Instance.new("Frame")
     PlayerFrame.BackgroundColor = Color3.fromRGB(94,94,94)
-    Instance.new("UICorner").Parent = PlayerFrame
+    local PlayerFrameUICorner = Instance.new("UICorner")
+    PlayerFrameUICorner.Parent = PlayerFrame
 end
 HolderFrame.Parent = BackgroundFrame
