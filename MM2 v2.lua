32f529d958f7e9380506ba58a870d4705fa357b7
CoolGermanGuy <EmailCGG@protonmail.com>
2025-03-17 20:07:36 +0100

    Update MM2 v2.lua

--- MM2 v2.lua
+++ MM2 v2.lua
@@ -11,6 +11,19 @@ ScreenGui.Parent = LocalPlayer.PlayerGui
 
 local BackgroundFrame = Instance.new("Frame") -- create BackgroundFrame
 BackgroundFrame.Name = "BackgroundFrame"
+BackgroundFrame.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
 Instance.new("UICorner").Parent = BackgroundFrame -- add UICorner
 BackgroundFrame.Parent = ScreenGui
 
+local HolderFrame = Instance.new("Frame")
+local UIGridLayout = Instance.new("UIGridLayout")
+UIGridLayout.CellPadding = {0, 10, 0, 7}
+UIGridLayout.CellSize = {0.5, -5, 0, 50}
+UIGridLayout.Parent = HolderFrame
+
+for i = 0, 12, 1 do
+    local PlayerFrame = Instance.new("Frame")
+    PlayerFrame.BackgroundColor = Color3.fromRGB(94,94,94)
+    Instance.new("UICorner").Parent = PlayerFrame
+end
+HolderFrame.Parent = BackgroundFrame
